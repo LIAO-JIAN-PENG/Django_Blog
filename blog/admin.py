32cea9ed7dac74160df64460b2694cf9cc22fe75
JPeng <alan36257407@gmail.com>
2021-07-26 13:04:04 +0800

    JP_modify

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Profile, Post
+
 
 # Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    list_display = ["title", "slug", "status", "created_date"]
+    list_filter = ["status"]
+    search_fields = ["title", "content"]
+    prepopulated_fields = {"slug": ("title",)}
+
+
+admin.site.register(Profile)
+admin.site.register(Post, PostAdmin)
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+# status for post
+STATUS = ((0, "Draft"), (1, "Publish"))
 
 # Create your models here.
+
+
+# model for profile
+class Profile(models.Model):
+    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
+    description = models.TextField()
+
+    def __str__(self):
+        return str(self.user)
+
+
+# model for post
+class Post(models.Model):
+    title = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=50, unique=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="blog_posts"
+    )
+    updated_date = models.DateTimeField(auto_now=True)
+    content = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    likes = models.IntegerField(default=0)
+
+    class Meta:
+        ordering = ["-created_date"]
+
+    def __str__(self):
+        return self.title
