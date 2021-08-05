18b30fb18b4bb3ef4e6c5d8c85495b21bdd87f4f
JPeng <alan36257407@gmail.com>
2021-08-05 23:59:25 +0800

    新增Like功能
    除錯: 我將slug功能取消，先暫時以uuid來儲存，並且更動url path中slug的判定，全改以post.id暫定

--- post/urls.py
+++ post/urls.py
@@ -1,9 +1,10 @@
-from .views import PostCreateView, PostDetailView, PostListView
-from django.urls import path, include
+from .views import PostCreateView, PostDetailView, PostListView, LikeView
+from django.urls import path
 
 urlpatterns = [
     # urls for post
     path("", PostListView.as_view(), name="post_list"),
     path("create/", PostCreateView.as_view(), name="post_create"),
-    path("<slug:slug>/", PostDetailView.as_view(), name="post_detail"),
+    path("<int:pk>/", PostDetailView.as_view(), name="post_detail"),
+    path('like/<int:pk>', LikeView, name="like_post"),
 ]
