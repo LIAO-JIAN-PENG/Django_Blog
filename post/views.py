d7bdf8a5ca698dc34bd729297949d0db55732b8e
JPeng <alan36257407@gmail.com>
2021-08-07 10:19:04 +0800

    Unlike button

--- post/views.py
+++ post/views.py
@@ -8,20 +8,26 @@ from .forms import PostForm
 
 def LikeView(request, pk):
     post = get_object_or_404(Post, id=request.POST.get("post_id"))
-    post.likes.add(request.user)
-    return HttpResponseRedirect(reverse("post-detail", args=[str(pk)]))
+    liked = False
+    if post.likes.filter(id=request.user.id).exists():
+        post.likes.remove(request.user)
+        liked = False
+    else:
+        post.likes.add(request.user)
+        liked = True
+    return render(reverse("post-detail", args=[str(pk)]))
 
 
 # views for post
 class PostListView(ListView):
+    model = Post
     template_name = "post/post_list.html"
-    queryset = Post.objects.all()
 
 
 class PostCreateView(CreateView):
+    model = Post
     template_name = "post/post_create.html"
     form_class = PostForm
-    queryset = Post.objects.all()
 
     # check if the form is valid
     def form_valid(self, form):
@@ -31,5 +37,19 @@ class PostCreateView(CreateView):
 
 
 class PostDetailView(DetailView):
+    model = Post
     template_name = "post/post_detail.html"
-    queryset = Post.objects.all()
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(PostDetailView, self).get_context_data()
+
+        stuff = get_object_or_404(Post, id=self.kwargs.get("pk"))
+        total_likes = stuff.likes.all().count()
+
+        liked = False
+        if stuff.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        context["total_likes"] = total_likes
+        context["liked"] = liked
+        return context
